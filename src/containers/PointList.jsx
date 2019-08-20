9232d090a50edb6c151233b393f129c987bc416a
softAdd <soft.snake867@gmail.com>
2019-08-20 17:45:01 +0500

    point styles

--- src/containers/PointList.jsx
+++ src/containers/PointList.jsx
@@ -1,11 +1,49 @@
 import React, { Component } from 'react';
 
 class PointList extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      inputValue: '',
+      points: [],
+    }
+  }
+
+  handleInputChange = event => {
+    this.setState({
+      inputValue: event.target.value,
+    });
+  }
+
+  handleInputKey = event => {
+    if (event.key === 'Enter') {
+      let text = event.target.value;
+      this.setState({
+        inputValue: '',
+        points: [...this.state.points, text],
+      })
+    }
+  }
+
   render() {
+    const { points } = this.state;
     return (
       <div className="point-list-container">
-        <input type="text" autoComplete="off" placeholder="enter point name" />
-        <div className="point-list"></div>
+        <input type="text"
+          autoComplete="off"
+          placeholder="press enter to add point"
+          value={this.state.inputValue}
+          onChange={this.handleInputChange}
+          onKeyPress={this.handleInputKey}
+        />
+        <div className="point-list">
+          {points.map((point, index) => (
+            <div className="point-item">
+              <p className="point-name">{point}</p>
+              <span class="point-delete" id={`point-delete-${index}`}>x</span>
+            </div>
+          ))}
+        </div>
       </div>
     )
   }
