b87aad67da05ec08b6fbe6ccbe34cd30b6dfb708
softAdd <soft.snake867@gmail.com>
2019-08-20 16:22:52 +0500

    on geometry change log coordinates

--- src/App.js
+++ src/App.js
@@ -4,14 +4,25 @@ import { YMaps, Map, Placemark } from 'react-yandex-maps';
 const { ymaps } = window;
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.myPlacemark = React.createRef();
+  }
   componentDidMount() {
     console.log(ymaps)
+  }
+  handleClick = () => {
+    console.log(ymaps)
+  }
+  handleChange = () => {
+    console.log(this)
   }
   render() {
     return (
       <YMaps>
+        <button onClick={this.handleClick}>Click to log</button>
         <Map defaultState={{ center: [55.75, 37.57], zoom: 11 }} style={{ height: '300px', width: '300px' }}>
-          <Placemark geometry={[55.75, 37.57]} options={{ draggable: true }} />
+          <Placemark geometry={[55.75, 37.57]} options={{ draggable: true }} onGeometryChange={() => { console.log(this.myPlacemark.current.geometry._coordinates) }} instanceRef={this.myPlacemark} />
         </Map>
       </YMaps>
     );
