0fe9ea6f3a5b1f58901c4504914493bb18c97e77
Maksim Konkov <soft.snake867@gmail.com>
2019-08-23 20:49:37 +0500

    log dropped if dropped

--- src/containers/yandex_map/List.jsx
+++ src/containers/yandex_map/List.jsx
@@ -1,10 +1,20 @@
 import React from 'react';
+import { useDrop } from 'react-dnd';
+import ItemTypes from './ItemTypes';
 
 import Mark from './Mark';
 
 const PointList = ({ marks, deletePoint }) => {
+  const [{ isOver, canDrop }, drop] = useDrop({
+    accept: ItemTypes.MARK,
+    drop: () => console.log('dropped'),
+    collect: mon => ({
+      isOver: !!mon.isOver(),
+      canDrop: !!mon.canDrop(),
+    }),
+  })
   return (
-    <div className="point-list">
+    <div className="point-list" ref={drop}>
       {marks.map((mark, index) => (
         <Mark key={`point-${index}`} mark={mark} deletePoint={() => { deletePoint(mark) }} />
       ))}
