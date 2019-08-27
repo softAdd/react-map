d5875fdc4209e9bbc2cfce5b862ba0e940e58b99
softAdd <soft.snake867@gmail.com>
2019-08-27 16:21:25 +0500

    api key field

--- src/components/yandex_map/YMap.jsx
+++ src/components/yandex_map/YMap.jsx
@@ -1,14 +1,15 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import PropTypes from 'prop-types';
 import { YMaps, Map, Placemark, Polyline } from 'react-yandex-maps';
 import axios from 'axios';
-
-const apikey = '';
 
 class YMap extends Component {
   constructor(props) {
     super(props);
     this.map = React.createRef();
+    this.state = {
+      apikey: '',
+    }
   }
 
   componentDidUpdate() {
@@ -21,7 +22,7 @@ class YMap extends Component {
   }
 
   setAddress = mark => {
-    axios.get(`https://geocode-maps.yandex.ru/1.x/?apikey=${apikey}&geocode=${mark.geometry[1]},${mark.geometry[0]}&results=1&format=json`)
+    axios.get(`https://geocode-maps.yandex.ru/1.x/?apikey=${this.state.apikey}&geocode=${mark.geometry[1]},${mark.geometry[0]}&results=1&format=json`)
 		.then(res => {
 		  const address = res.data.response.GeoObjectCollection.featureMember[0].GeoObject.name;
 		  if (address) {
@@ -42,43 +43,56 @@ class YMap extends Component {
         return mark.geometry;
       }
       return this.map.current.getCenter();
-    })
-  }
+    });
+	}
+	
+	changeApiKey = event => {
+    this.setState({
+      apikey: event.target.value
+    });
+  };
 
   render() {
     const { marks, mapDefaultState, mapWidth, mapHeight } = this.props;
     return (
-      <div className="yandex-map-container">
-        <YMaps>
-          <Map defaultState={mapDefaultState} style={{ height: mapHeight, width: mapWidth }} instanceRef={this.map}>
-            {marks.map((mark, index) => (
-              <Placemark 
-                geometry={mark.geometry}
-                options={{ draggable: true }} 
-                key={`${mark.title}-${index}`} 
-                onDragEnd={event => { this.onDragEnd(event, mark) }}
-                modules={['geoObject.addon.balloon', 'geoObject.addon.hint']}
-                properties={{
-                  hintContent: mark.address,
-                  balloonContent: `<p>${mark.title}</p><p>${mark.address}</p>`,
-                }}
-              />
-            ))}
-            <Polyline
-              geometry={{
-                type: 'LineString',
-                coordinates: this.getAllCoordinates(),
-              }}
-              options={{
-                strokeWidth: 4,
-                strokeColor: '000000',
-                draggable: false,
-                strokeOpacity: 0.5,
-              }}
-            />
-          </Map>
-        </YMaps>
-      </div>
+			<Fragment>
+				<div className="yandex-map-container">
+					<YMaps>
+						<Map defaultState={mapDefaultState} style={{ height: mapHeight, width: mapWidth }} instanceRef={this.map}>
+							{marks.map((mark, index) => (
+								<Placemark 
+									geometry={mark.geometry}
+									options={{ draggable: true }} 
+									key={`${mark.title}-${index}`} 
+									onDragEnd={event => { this.onDragEnd(event, mark) }}
+									modules={['geoObject.addon.balloon', 'geoObject.addon.hint']}
+									properties={{
+										hintContent: mark.address,
+										balloonContent: `<p>${mark.title}</p><p>${mark.address}</p>`,
+									}}
+								/>
+							))}
+							<Polyline
+								geometry={{
+									type: 'LineString',
+									coordinates: this.getAllCoordinates(),
+								}}
+								options={{
+									strokeWidth: 4,
+									strokeColor: '000000',
+									draggable: false,
+									strokeOpacity: 0.5,
+								}}
+							/>
+						</Map>
+					</YMaps>
+				</div>
+				<div className="api">
+					<p>API_KEY:</p>
+					<input onChange={event => { this.changeApiKey(event) }}/>
+        </div>
+			</Fragment>
+
     );
   }
 }
