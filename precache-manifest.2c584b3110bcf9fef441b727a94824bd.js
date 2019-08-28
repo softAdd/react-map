6e6359b486b0a714c8a88cb2d1b90f6ec03b3ced
softAdd <soft.snake867@gmail.com>
2019-08-28 14:04:26 +0500

    Updates

---
+++ precache-manifest.2c584b3110bcf9fef441b727a94824bd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b2d66c5d89926c9276f6f7c864ffd1d",
+    "url": "/react-map/index.html"
+  },
+  {
+    "revision": "27f046f0526cc93a95b0",
+    "url": "/react-map/static/css/main.0b9889c2.chunk.css"
+  },
+  {
+    "revision": "0d3ddf8ab9c7588de0e3",
+    "url": "/react-map/static/js/2.5e792b66.chunk.js"
+  },
+  {
+    "revision": "27f046f0526cc93a95b0",
+    "url": "/react-map/static/js/main.90a73e03.chunk.js"
+  },
+  {
+    "revision": "ccdc9473542759e8b856",
+    "url": "/react-map/static/js/runtime~main.2f94919f.js"
+  }
+]);
